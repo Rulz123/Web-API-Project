a85cac664a861ab3df633166acf4d5c41f6e6575
Rulz123 <75121436+Rulz123@users.noreply.github.com>
2021-10-01 11:21:01 +0200

    Added games class, and made database migration and connection string
    Added games class with all the information it needs, aswell as made data migration to create database table and rows via entity framework, and then added a connection string too.

---
+++ Obligatorisk opgave API MVC/Obligatorisk opgave API MVC/Migrations/20211001091706_AddGamesToDb.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Obligatorisk_opgave_API_MVC.Migrations
+{
+    public partial class AddGamesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    internalName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    metacriticlink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    dealID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    storeID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gameID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    salePrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    normalPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isOnSale = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    savings = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    metacriticscore = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    steamRatingText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    steamRatingPercent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    steamRatingCount = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    steamAppID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    releaseDate = table.Column<int>(type: "int", nullable: false),
+                    lastChange = table.Column<int>(type: "int", nullable: false),
+                    dealRating = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    thumb = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
---
+++ Obligatorisk opgave API MVC/Obligatorisk opgave API MVC/Models/Games.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_opgave_API_MVC
+{
+    public class Games
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string internalName { get; set; }
+        public string title { get; set; }
+        public string metacriticlink { get; set; }
+        public string dealID { get; set; }
+        public string storeID { get; set; }        
+        public string gameID { get; set; }
+        public string salePrice { get; set; }
+        public string normalPrice { get; set; }
+        public string isOnSale { get; set; }
+        public string savings { get; set; }
+        public string metacriticscore { get; set; }
+        public string steamRatingText { get; set; }
+        public string steamRatingPercent { get; set; }
+        public string steamRatingCount { get; set; }
+        public string steamAppID { get; set; }
+        public int releaseDate { get; set; }
+        public int lastChange { get; set; }
+        public string dealRating { get; set; }
+        public string thumb { get; set; }
+    }
+}
